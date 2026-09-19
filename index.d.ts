f9ed905e790f8409c9cbe6b8c8db5d9758949984
Durgaprasadbo <100009922+Durgaprasadbo@users.noreply.github.com>
2026-09-19 19:31:59 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,157 +1,28 @@
-//#region src/composable-filters.d.ts
-type StringOrRegExp = string | RegExp;
-type PluginModuleType = "js" | "jsx" | "ts" | "tsx" | "json" | "text" | "base64" | "dataurl" | "binary" | "empty" | (string & {});
-type FilterExpressionKind = FilterExpression["kind"];
-type FilterExpression = And | Or | Not | Id | ModuleType | Code | Query;
-type TopLevelFilterExpression = Include | Exclude;
-declare class And {
-  kind: "and";
-  args: FilterExpression[];
-  constructor(...args: FilterExpression[]);
-}
-declare class Or {
-  kind: "or";
-  args: FilterExpression[];
-  constructor(...args: FilterExpression[]);
-}
-declare class Not {
-  kind: "not";
-  expr: FilterExpression;
-  constructor(expr: FilterExpression);
-}
-interface QueryFilterObject {
-  [key: string]: StringOrRegExp | boolean;
-}
-interface IdParams {
-  cleanUrl?: boolean;
-}
-declare class Id {
-  kind: "id";
-  pattern: StringOrRegExp;
-  params: IdParams;
-  constructor(pattern: StringOrRegExp, params?: IdParams);
-}
-declare class ModuleType {
-  kind: "moduleType";
-  pattern: PluginModuleType;
-  constructor(pattern: PluginModuleType);
-}
-declare class Code {
-  kind: "code";
-  pattern: StringOrRegExp;
-  constructor(expr: StringOrRegExp);
-}
-declare class Query {
-  kind: "query";
-  key: string;
-  pattern: StringOrRegExp | boolean;
-  constructor(key: string, pattern: StringOrRegExp | boolean);
-}
-declare class Include {
-  kind: "include";
-  expr: FilterExpression;
-  constructor(expr: FilterExpression);
-}
-declare class Exclude {
-  kind: "exclude";
-  expr: FilterExpression;
-  constructor(expr: FilterExpression);
-}
-declare function and(...args: FilterExpression[]): And;
-declare function or(...args: FilterExpression[]): Or;
-declare function not(expr: FilterExpression): Not;
-declare function id(pattern: StringOrRegExp, params?: IdParams): Id;
-declare function moduleType(pattern: PluginModuleType): ModuleType;
-declare function code(pattern: StringOrRegExp): Code;
-declare function query(key: string, pattern: StringOrRegExp | boolean): Query;
-declare function include(expr: FilterExpression): Include;
-declare function exclude(expr: FilterExpression): Exclude;
+import { BrowserVersion, Options, AllVersionsOptions, AllBrowsersBrowserVersion, NestedBrowserVersions, TimelineOptions, TimelineEvent, BrowserTimeline } from "./types.js";
+export declare function _resetHasWarned(): void;
 /**
-* convert a queryObject to FilterExpression like
-* ```js
-*   and(query(k1, v1), query(k2, v2))
-* ```
-* @param queryFilterObject The query filter object needs to be matched.
-* @returns a `And` FilterExpression
-*/
-declare function queries(queryFilter: QueryFilterObject): And;
-declare function interpreter(exprs: TopLevelFilterExpression | TopLevelFilterExpression[], code?: string, id?: string, moduleType?: PluginModuleType): boolean;
-interface InterpreterCtx {
-  urlSearchParamsCache?: URLSearchParams;
-}
-declare function interpreterImpl(expr: TopLevelFilterExpression[], code?: string, id?: string, moduleType?: PluginModuleType, ctx?: InterpreterCtx): boolean;
-declare function exprInterpreter(expr: FilterExpression, code?: string, id?: string, moduleType?: PluginModuleType, ctx?: InterpreterCtx): boolean;
-//#endregion
-//#region src/simple-filters.d.ts
+ * Returns browser versions compatible with specified Baseline targets.
+ * Defaults to returning the minimum versions of the core browser set that support Baseline Widely available.
+ * Takes an optional configuration `Object` with four optional properties:
+ * - `listAllCompatibleVersions`: `false` (default) or `true`
+ * - `includeDownstreamBrowsers`: `false` (default) or `true`
+ * - `widelyAvailableOnDate`: date in format `YYYY-MM-DD`
+ * - `targetYear`: year in format `YYYY`
+ * - `supressWarnings`: `false` (default) or `true`
+ */
+export declare function getCompatibleVersions(userOptions?: Options): BrowserVersion[];
 /**
-* Constructs a RegExp that matches the exact string specified.
-*
-* This is useful for plugin hook filters.
-*
-* @param str the string to match.
-* @param flags flags for the RegExp.
-*
-* @example
-* ```ts
-* import { exactRegex } from '@rolldown/pluginutils';
-* const plugin = {
-*   name: 'plugin',
-*   resolveId: {
-*     filter: { id: exactRegex('foo') },
-*     handler(id) {} // will only be called for `foo`
-*   }
-* }
-* ```
-*/
-declare function exactRegex(str: string, flags?: string): RegExp;
-/**
-* Constructs a RegExp that matches a value that has the specified prefix.
-*
-* This is useful for plugin hook filters.
-*
-* @param str the string to match.
-* @param flags flags for the RegExp.
-*
-* @example
-* ```ts
-* import { prefixRegex } from '@rolldown/pluginutils';
-* const plugin = {
-*   name: 'plugin',
-*   resolveId: {
-*     filter: { id: prefixRegex('foo') },
-*     handler(id) {} // will only be called for IDs starting with `foo`
-*   }
-* }
-* ```
-*/
-declare function prefixRegex(str: string, flags?: string): RegExp;
-type WidenString<T> = T extends string ? string : T;
-/**
-* Converts a id filter to match with an id with a query.
-*
-* @param input the id filters to convert.
-*
-* @example
-* ```ts
-* import { makeIdFiltersToMatchWithQuery } from '@rolldown/pluginutils';
-* const plugin = {
-*   name: 'plugin',
-*   transform: {
-*     filter: { id: makeIdFiltersToMatchWithQuery(['**' + '/*.js', /\.ts$/]) },
-*     // The handler will be called for IDs like:
-*     // - foo.js
-*     // - foo.js?foo
-*     // - foo.txt?foo.js
-*     // - foo.ts
-*     // - foo.ts?foo
-*     // - foo.txt?foo.ts
-*     handler(code, id) {}
-*   }
-* }
-* ```
-*/
-declare function makeIdFiltersToMatchWithQuery<T extends string | RegExp>(input: T): WidenString<T>;
-declare function makeIdFiltersToMatchWithQuery<T extends string | RegExp>(input: readonly T[]): WidenString<T>[];
-declare function makeIdFiltersToMatchWithQuery(input: string | RegExp | readonly (string | RegExp)[]): string | RegExp | (string | RegExp)[];
-//#endregion
-export { FilterExpression, FilterExpressionKind, QueryFilterObject, TopLevelFilterExpression, and, code, exactRegex, exclude, exprInterpreter, id, include, interpreter, interpreterImpl, makeIdFiltersToMatchWithQuery, moduleType, not, or, prefixRegex, queries, query };
+ * Returns all browser versions known to this module with their level of Baseline support as a JavaScript `Array` (`"array"`), `Object` (`"object"`) or a CSV string (`"csv"`).
+ * Takes an optional configuration `Object` with three optional properties:
+ * - `includeDownstreamBrowsers`: `false` (default) or `true`
+ * - `outputFormat`: `"array"` (default), `"object"` or `"csv"`
+ * - `useSupports`: `false` (default) or `true`, replaces `wa_compatible` property with optional `supports` property which returns `widely` or `newly` available when present.
+ * - `supressWarnings`: `false` (default) or `true`
+ */
+export declare function getAllVersions(userOptions?: AllVersionsOptions): AllBrowsersBrowserVersion[] | NestedBrowserVersions | string;
+export declare function getTimeline(userOptions?: TimelineOptions & {
+    groupBy?: "date";
+}): TimelineEvent[];
+export declare function getTimeline(userOptions: TimelineOptions & {
+    groupBy: "browser";
+}): BrowserTimeline;
